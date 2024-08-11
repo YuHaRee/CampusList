1be1bd01bb55e4175d1833581076dba175862857
PCH <paekrng7@naver.com>
2024-08-11 19:42:11 +0900

    refactor: camping CampingService 수정

--- src/main/java/com/commit/campuslist1/service/CampingService.java
+++ src/main/java/com/commit/campuslist1/service/CampingService.java
@@ -1,8 +1,13 @@
 package com.commit.campuslist1.service;
 
-import com.commit.campuslist1.dto.CampingDTO;
+import com.commit.campus.dto.CampingDTO;
+
+import java.util.List;
 
 public interface CampingService {
+    List<CampingDTO> getAllCampings();  // 모든 캠핑장 정보를 조회하는 메서드
+    CampingDTO createCamping(CampingDTO campingDTO);  // 새로운 캠핑장을 생성하는 메서드
+
     // 오프셋 기반 페이지네이션을 사용하여 캠핑 정보를 가져오는 메서드
     CampingDTO getCampings(String doName, String sigunguName, int page, int size, String sort, String order);
 }
